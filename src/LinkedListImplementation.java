4b159eecb12cef35a9e3d3087e9520bc9ab6152e
subhodipray <raysubhodip1993gmail.com>
2022-07-01 02:12:59 -0400

    Java LinkedList Add elements

--- src/LinkedListImplementation.java
+++ src/LinkedListImplementation.java
@@ -1,7 +1,9 @@
 import java.util.LinkedList;
+import java.util.Iterator;
 public class LinkedListImplementation {
     public static void main (String [] args)
     {
+        //LinkedList Add the Entries into the List
         LinkedList<String> L1 = new LinkedList<>();
         L1.add("Entry 1");
         L1.add("Entry 2");
@@ -10,6 +12,39 @@ public class LinkedListImplementation {
 
 
         System.out.println(L1);
+
+        //LinkedList Add Elements to the Beginning of the LinkedList
+        L1.add(0, "Entry 0");
+
+        System.out.println("List after adding to the first position---");
+        System.out.println(L1);
+
+        //LinkedList Size
+        System.out.println("The size of the LinkedList is : " + L1.size());
+
+        // Return an Array of the elements of the Linkedlist - First to Last
+        String [] arr = new String[L1.size()];
+        L1.toArray(arr);
+
+        System.out.println("Print form an array converted from the LinkedList---");
+
+        int i =0;
+        for(String s : arr){
+
+            System.out.println("Array at the "+i+"th position is : "+s);
+            i++;
+        }
+
+        //reverse a linked List
+        System.out.println("Reverse a LinkedList ---");
+        Iterator<String> obj = L1.descendingIterator();
+
+        while(obj.hasNext()){
+
+            System.out.println(obj.next());
+        }
+
+
     }
 
 }
