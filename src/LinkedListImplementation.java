259ee11d77086415301fa918119691b7429d766d
subhodipray <raysubhodip1993gmail.com>
2022-07-01 01:12:07 -0400

    Java LinkedList Add elements

---
+++ src/LinkedListImplementation.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+public class LinkedListImplementation {
+    public static void main (String [] args)
+    {
+        LinkedList<String> L1 = new LinkedList<>();
+        L1.add("Entry 1");
+        L1.add("Entry 2");
+        L1.add("Entry 3");
+        L1.add("Entry 4");
+
+
+        System.out.println(L1);
+    }
+
+}
